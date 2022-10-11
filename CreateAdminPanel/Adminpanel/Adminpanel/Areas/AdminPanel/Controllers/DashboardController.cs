e120cf01e0112ff62a7c6b7a022816252d2deb22
saidsn <saidsn@code.edu.az>
2022-10-11 09:00:46 +0400

    Admin-Panel

---
+++ CreateAdminPanel/Adminpanel/Adminpanel/Areas/AdminPanel/Controllers/DashboardController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Adminpanel.Areas.AdminPanel.Controllers
+{
+    public class DashboardController : Controller
+    {
+
+        [Area("AdminPanel")]
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+   
+    
+     
+    }
+}
